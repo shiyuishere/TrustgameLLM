7eee3605ee4e50a6d7a4cf7055bffed0aed948bc
Shiyu <122246960+shiyuishere@users.noreply.github.com>
2026-05-13 15:56:58 +0200

    Update comments and output messages in run_all.py

--- run_all.py
+++ run_all.py
@@ -1,7 +1,7 @@
 import subprocess
 
 commands = [
-    # --- oneshot 部分 ---
+    # oneshot
     'uv run oneshot.py --llm-config configs/llms/deepseek.yaml --prompt-config configs/prompts/oneshot/fr.yaml --output "/Users/fancy/Documents/Utrecht SaSR/My thesis/AI Experiments/trustgame/trustgame_0912/results/FR/OS/oneshot_20250903_114621.csv"',
     'uv run oneshot.py --llm-config configs/llms/deepseek.yaml --prompt-config configs/prompts/oneshot/fr.yaml --output "/Users/fancy/Documents/Utrecht SaSR/My thesis/AI Experiments/trustgame/trustgame_0912/results/FR/OS/oneshot_20250903_122926.csv"',
     'uv run oneshot.py --llm-config configs/llms/openai.yaml --prompt-config configs/prompts/oneshot/zh.yaml --output "/Users/fancy/Documents/Utrecht SaSR/My thesis/AI Experiments/trustgame/trustgame_0912/results/ZH/OS/oneshot_20250903_152146.csv"',
@@ -9,7 +9,7 @@ commands = [
     'uv run oneshot.py --llm-config configs/llms/openai.yaml --prompt-config configs/prompts/oneshot/zh.yaml --output "/Users/fancy/Documents/Utrecht SaSR/My thesis/AI Experiments/trustgame/trustgame_0912/results/ZH/OS/oneshot_20250903_155001.csv"',
     'uv run oneshot.py --llm-config configs/llms/deepseek.yaml --prompt-config configs/prompts/oneshot/zh.yaml --output "/Users/fancy/Documents/Utrecht SaSR/My thesis/AI Experiments/trustgame/trustgame_0912/results/ZH/OS/oneshot_20250903_180255.csv"',
 
-    # --- check_experiments 部分 ---
+    # check_experiments 
     'uv run ./check_experiments.py --result "/Users/fancy/Documents/Utrecht SaSR/My thesis/AI Experiments/trustgame/trustgame_0912/results/FR/OS/oneshot_20250903_114621.csv" --llm-config configs/llms/deepseek.yaml --prompt-config configs/prompts/oneshot/fr.yaml',
     'uv run ./check_experiments.py --result "/Users/fancy/Documents/Utrecht SaSR/My thesis/AI Experiments/trustgame/trustgame_0912/results/FR/OS/oneshot_20250903_122926.csv" --llm-config configs/llms/deepseek.yaml --prompt-config configs/prompts/oneshot/fr.yaml',
     'uv run ./check_experiments.py --result "/Users/fancy/Documents/Utrecht SaSR/My thesis/AI Experiments/trustgame/trustgame_0912/results/ZH/OS/oneshot_20250903_152146.csv" --llm-config configs/llms/openai.yaml --prompt-config configs/prompts/oneshot/zh.yaml',
@@ -28,12 +28,11 @@ for i, cmd in enumerate(commands, 1):
     else:
         print(f"❌ Command {i} failed with code {result.returncode}")
 
-# --- 运行完成后的汇总 ---
 print("\n==============================")
 if failed_cmds:
-    print("以下命令执行失败：")
+    print("The following command failed to execute：")
     for c in failed_cmds:
         print(c)
 else:
-    print("🎉 所有命令都执行成功！")
+    print("All commands were executed successfully!")
 print("==============================")
